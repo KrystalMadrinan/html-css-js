ac5e65921991572abf5b1280e4ab431f5d0e6923
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2019-09-20 13:31:42 -0700

    to see working js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-function askName(){
-    var userName = prompt('Hello, what is your name?'); 
+function askName() {
+    var userName = prompt('Hello, what is your name?');
     return '<h3>' + 'Hello ' + userName + '!' + '</h3>'
 }
 
-function askQuestion(){
+function askQuestion() {
     var answer = prompt('Are alpacas cute?');
     var response;
+
+    while (answer === '') {
+        answer = prompt('Are alpacas cute?');
+    }
+
+    if (answer === 'yes') {
+        response = 'Yes they are!';
+    } else if (answer === 'no') {
+        response = 'That is fine!';
+    } else {
+        response = 'Cool story but yes or no though??';
+    }
+    return '<h3>' + response + '</h3>';
+
 }
 
-while(answer === ''){
-    answer = prompt('Are alpacas cute?');
- } 
-
-if(answer === 'yes') {
-    response = 'Yes they are!';
-}  else if(answer === 'no'){
-    response = 'That is fine!';
-}  else {
-    response = 'Cool story but yes or no though??';
-}
-
-return '<h3>' + response + '</h3>'; 
-
-function showAmount(){
+function showAmount() {
     var answer = prompt('How many alpacas do you want? Enter a number.');
-
-    for(var i = 0, i < result, i++){
-        var result = showAmount();
+    var result = '';
+    for (var i = 0; i < answer; i++) {
+        result = result + '#alpaca';
     }
     return result;
 }
